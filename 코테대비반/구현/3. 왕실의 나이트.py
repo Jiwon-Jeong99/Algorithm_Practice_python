f5d159222b6b0ca0d24288f9fdf86644cdf353da
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-01-07 17:00:37 +0900

    왕실의 나이트

---
+++ 코테대비반/구현/3. 왕실의 나이트.py
@@ -0,0 +1,19 @@
+#강사님 방법
+g = input()
+count = 0
+
+dic_change = {"a":1,"b":2,"c":3,"d":4,"e":5,"f":6,"g":7,"h":8}
+x,y = dic_change[g[0]],int(g[1])
+
+move_root = [(2,1),(1,2),(-1,2),(-2,1),(-2,-1),(-1,-2),(1,-2),(2,-1)]
+
+for i in move_root:
+  print(i)
+  x_d = x + i[0]
+  y_d = y + i[1]
+  print("[chk] x_d,y_d : ",x_d,y_d)
+  if x_d >= 1 and x_d <= 8 and y_d >= 1 and y_d <= 8:
+    count += 1
+    print("count += :",count,"\n")
+
+print (count)
