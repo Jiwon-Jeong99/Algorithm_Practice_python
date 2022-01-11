f144013f2c6ccc68bd805b993232f071a1ebfdae
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-01-11 13:30:47 +0900

    탐색 개념

---
+++ 코테대비반/탐색/개념.py
@@ -0,0 +1,14 @@
+'''
+DFS(깊이우선탐색)
+최대한 멀리 있는 노드를 탐색
+유형-미로, 한쪽 방향 이동
+Stack: 선입후출/박스쌓기
+-append(), pop()
+
+BFS(너비우선탐색)
+가까이 있는 노드를 먼저 탐색
+유형-주변을 다 확인
+Queue: 선입선출/줄서기
+-from collections import deque
+append(), popleft()
+'''
