d0a342d961bd18a5b290683b21b1c66cdd5ae986
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-01-12 14:24:01 +0900

    선택정렬

--- 코테대비반/정렬/선택정렬.py
+++ 코테대비반/정렬/선택정렬.py
@@ -1,7 +1,7 @@
 array = [7,5,9,0,3,1,6,2,4,8]
 
-for i in range(len(array)):
-    min_index = i
+for i in range(len(array)): #전체 리스트
+    min_index = i #min_a 비교 기준 위치 값  
     for j in range(i+1,len(array)):
         if array[min_index]>array[j]:
             min_index=j
