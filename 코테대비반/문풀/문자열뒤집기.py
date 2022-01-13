5a49f056299f33baa11f90aa90cf40344909772a
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-01-14 01:02:39 +0900

    문자열뒤집기

--- 코테대비반/문풀/문자열뒤집기.py
+++ 코테대비반/문풀/문자열뒤집기.py
@@ -1,5 +1,23 @@
+#더하거나 0 1 +
+#곱하거나 2 9 x
+
 import sys
-
 n = sys.stdin.readline()
+result = 0
 
-print(n)
+for i in range(int(len(n))-1):
+  print("i :",i,n[i])
+
+  if i == 0:
+    result = int(n[i])
+    print("result = n[i] :",result,n[i])
+    continue
+
+  if int(n[i]) == 0 or int(n[i]) == 1 or result == 0:
+    result += int(n[i])
+    print("덧셈 :",result,"+=",i,int(n[i]))
+  else:
+    result *= int(n[i])
+    print("곱셈 :",result,"*=",i,int(n[i]))
+
+print(result)
