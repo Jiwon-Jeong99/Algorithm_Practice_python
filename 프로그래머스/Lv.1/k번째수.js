a5fa1fdbd3ca252230dbb41f1db237352d0d1eaa
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-02-16 21:02:37 +0900

    k번째수.js

---
+++ 프로그래머스/Lv.1/k번째수.js
@@ -0,0 +1,7 @@
+function solution(array, commands){
+    let answer = [];
+    for(let i=0; i<commands.length;i++){
+        let eachCommand = commands[i];
+        let slice = array.slice(eachCommand[0]-1,)
+    }
+}
