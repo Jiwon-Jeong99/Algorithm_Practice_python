c399d1af011bea4f10e5e3b3cc1ee9ad5ab2f49f
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-02-23 12:26:04 +0900

    boj 10814

---
+++ 백준/10814.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+agree =[]
+for i in range(n):
+    age, name = sys.stdin.readline().split()
+    agree.append([age,name])
+
+# [age,name]에서 age만 비교
+agree.sort(key = lambda x : x[0])
+
+# [[a,b],[c,d]] 중 j는 [a,b]임 
+for j in agree:
+    print(j[0],j[1])
--- 백준/1931.py
+++ 백준/1931.py
@@ -3,6 +3,6 @@ import sys
 n = int(sys.stdin.readline())
 time_list=[]
 for i in range(n-1):
-    a,b = map(int,sys.stdin.readline().split())
-    time_list.append([a,b])
+    start,end = map(int,sys.stdin.readline().split())
+    time_list.append([start,end])
 
