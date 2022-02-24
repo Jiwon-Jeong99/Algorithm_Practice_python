f09b4ceaa2dfba132bdd22dfc9bcf0f832a4ab7d
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-02-25 01:44:03 +0900

    2108

---
+++ 백준/2108.py
@@ -0,0 +1,18 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+num_list = []
+count_list = []
+for i in range(n):
+    num_list.append(int(sys.stdin.readline()))
+
+num_set = set(num_list)
+sorted(num_set)
+for j in num_set:
+    count_list.append(num_list.count(j))
+    
+a = round(sum(num_list) / n)
+b = num_list[(n-1)/2]
+c = count_list[1]
+d = max(num_list) - min(num_list)
