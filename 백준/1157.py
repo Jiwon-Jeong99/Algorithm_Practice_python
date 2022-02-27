3b14f9ba4b941d8099ff020a9fc69f7b3c70b4c1
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-02-27 20:36:22 +0900

    boj 1157

--- 백준/1157.py
+++ 백준/1157.py
@@ -1,2 +1,20 @@
 import sys
 
+a = sys.stdin.readline().upper()
+
+# 입력받은 문자열에서 중복값 제거
+a_set = list(set(a))
+
+answer = []
+for i in a_set:
+    b = a.count(i)
+    answer.append(b)
+
+# 최대값이 여러개라면
+if answer.count(max(answer)) > 1:
+    print('?')
+
+else:
+    index_num = answer.index(max(answer))
+    print(a_set[index_num])
+
