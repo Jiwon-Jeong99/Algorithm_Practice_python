380ef470e89ca6e64e2787ad9eaa44bf5fe67d46
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-07-09 07:58:22 +0900

    boj 1931

--- boj/정렬/1931.py
+++ boj/정렬/1931.py
@@ -9,3 +9,13 @@ for _ in range(n):
 
 # 회의를 최대로 잡으려면 회의시간이 짧은 걸 공략
 list.sort(key=lambda x:(x[1],x[0]))
+
+end = list[0][1]
+cnt = 0
+
+for i in range(1,n):
+    if list[i][0] >= end:
+        cnt += 1
+        end = list[i][1]
+
+print(cnt)
