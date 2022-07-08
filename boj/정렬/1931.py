90dd78e2c8b7ae14f4b87d73640d9d25112b183c
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-07-09 02:42:23 +0900

    boj 1931

--- boj/정렬/1931.py
+++ boj/정렬/1931.py
@@ -1,5 +1,11 @@
 import sys
 
+list = []
+
 n = int(sys.stdin.readline())
 for _ in range(n):
     a,b = list(map(int,sys.stdin.readline().split()))
+    list.append([a,b])
+
+# 회의를 최대로 잡으려면 회의시간이 짧은 걸 공략
+list.sort(key=lambda x:(x[1],x[0]))
