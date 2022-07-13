1d126d4a738c7d6eb3795cf9d6b4a0de1b38c175
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-07-14 02:06:19 +0900

    boj 1622

--- boj/스택/1662.py
+++ boj/스택/1662.py
@@ -1,1 +1,19 @@
 import sys
+
+s = list(sys.stdin.readline().rstrip())
+
+cnt = 0
+length = 0
+
+while s:
+    if s[-1] == ')':
+        s.pop()
+    elif s[-1].isdigit():
+        length += 1
+        s.pop()
+    
+
+
+
+
+
