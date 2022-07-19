5b09f4e7dd33df9ff84a597857f8cb9cc469b273
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-07-20 08:49:38 +0900

    boj 17503

---
+++ boj/큐/17503.py
@@ -0,0 +1,9 @@
+import sys
+from collections import deque
+
+deq = deque()
+day, total_like, kind = deque(map(int,sys.stdin.readline().split()))
+for _ in range(k):
+    like, alch = map(int, sys.stdin.readline().split())
+    
+
