811929f7ee4072ee80b6a80d177935abcf107057
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-03-13 23:06:18 +0900

    튜플.py

---
+++ 카카오/크레인 인형뽑기 게임.py
@@ -0,0 +1,21 @@
+# 
+import sys
+
+def solution(board, moves):
+    answer = 0
+    stack = []
+    
+    for move in moves:
+        for i in range(len(board)):
+            if board[i][move-1] != 0:
+                stack.append(board[i][move-1])
+                board[i][move-1] = 0
+            
+            if len(stack) >= 2:
+                if stack[-1] == stack[-2]:
+                    stack.pop()
+                    stack.pop()
+                    answer += 2
+            break
+    
+    return answer
