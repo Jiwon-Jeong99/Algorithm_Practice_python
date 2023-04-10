f3d6aee0565c69bca6a82e82b69818eba41c4d74
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2023-04-10 18:15:47 +0900

    신규아이디추천.py

---
+++ study/카카오/신규아이디추천.py
@@ -0,0 +1,50 @@
+import sys
+
+new_id = sys.stdin.readline().rstrip()
+import re
+
+def to_lower(string):
+    return string.lower();
+
+def remove_special(string):
+    regex = r'[^a-z0-9\-_.]'
+    return re.sub(regex,'',string)
+
+def combine_dot(string):
+    regex = r'\.{2,}'
+    return re.sub(regex, '.', string)
+        
+def remove_fbdot(string):
+    regex = r'^\.|\.$'
+    return re.sub(regex, '', string)
+
+def add_a(string):
+    if string == '':
+        return(string.join('a'))
+    else:
+        return string
+
+def remove_extra_string(string):
+    if len(string) >= 16:
+        string = string[:15]
+        return re.sub(r'\.$','',string)
+    else:
+        return string
+
+def repeat_last_string(string):
+    while len(string) < 3 :
+        string += string[len(string)-1]
+    return string
+
+def solution(new_id):
+    a = to_lower(new_id)
+    b = remove_special(a)
+    c = combine_dot(b)
+    d = remove_fbdot(c)
+    e = add_a(d)
+    f = remove_extra_string(e)
+    answer = repeat_last_string(f)
+    
+    return answer
+
+solution(new_id)
