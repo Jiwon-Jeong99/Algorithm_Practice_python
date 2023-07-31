f1de408a150c14d6633a5fa9b06b15da4b37eb2c
Jiwon Jeong <jjwzzy4643@gmail.com>
2023-08-01 00:38:37 +0900

    Create swea_2046.java

---
+++ ssafy/SWexpert/swea_2046.java
@@ -0,0 +1,13 @@
+package swea;
+
+import java.util.Scanner;
+public class swea_2046 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int testCase = scan.nextInt();
+
+        for(int tc=1; tc<=testCase; tc++){ //입력한 수만큼 # 프린트
+            System.out.print("#");
+        }
+    }
+}
