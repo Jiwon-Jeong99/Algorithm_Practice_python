9cc1f9f447fe2c8208448eb998fc000123121e7b
Jiwon Jeong <jjwzzy4643@gmail.com>
2023-08-14 01:55:50 +0900

    Add files via upload

--- ssafy/boj/boj_2493.java
+++ ssafy/boj/boj_2493.java
@@ -3,7 +3,6 @@ package exercise;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -18,30 +17,42 @@ import java.util.StringTokenizer;
  */
 
 public class boj_2493 {
-	static BufferedReader br;
-	static StringTokenizer st;
-	static int total; //탑 총 개수
-	static Stack<Integer> towers; //탑의 높이를 담은 큐
-	static int[] towersCnt; //탑 별 레이저 받은 탑 번호
-	
-	public static void main(String[] args) throws IOException {
-		br = new BufferedReader(new InputStreamReader(System.in));
-		st = new StringTokenizer(br.readLine().trim());
-		
-		total = Integer.parseInt(br.readLine().trim()); //탑 총 개수 입력
-		
-		towers = new Stack<Integer>();
-		
-		for(int idx=0; idx<total; idx++) { //탑의 높이 담은 배열 만들기
-			int tower = Integer.parseInt(st.nextToken());
-			
-			if(towers.isEmpty()) {
-				towers.add(tower);
-				towersCnt[idx] = 0;
-			} else if(towers.peek() > tower) {
-				towers.add(tower);
-				
-			}
-		}
-	}
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int total; //탑 총 개수
+    static Stack<Integer> towers; //탑의 높이를 담은 큐
+    static int[] towersCnt; //탑 별 정보를 담을 배열
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+
+        total = Integer.parseInt(br.readLine().trim()); //탑 총 개수 입력
+
+        towers = new Stack<>();
+        towersCnt = new int[total+1]; //번호를 담을 배열
+
+        int[] arr = new int[total+1]; //탑 정보를 담을 배열
+        st = new StringTokenizer(br.readLine().trim());
+        for(int index=1; index<=total; index++){
+            arr[index] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int idx=1; idx<=total; idx++) { //레이저 맞은 탑 비교하기
+            while(towers.size()>0){
+                if(arr[towers.peek()] >= arr[idx]){ //스택상단에 있는 탑이 더 클 때
+                    towersCnt[idx] = towers.peek();
+                    break;
+                }else{
+                    towers.pop(); //넣으려고 하는 탑이 더 클때
+                }
+            }
+            towers.push(idx);
+        }
+        sb = new StringBuilder();
+        for(int ansIdx=1; ansIdx<=total; ansIdx++){
+            sb.append(towersCnt[ansIdx]).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
 }
